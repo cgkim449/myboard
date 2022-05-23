b5a5cda016f7c5dce5833ad5f32a674e68072294
Changu Kim <cgkim449@gmail.com>
2022-05-23 22:33:29 +0900

    feat: 게시물 목록 api 추가 (#10)

---
+++ backend/myboard/src/main/java/com/cgkim/myboard/vo/board/BoardSearchRequest.java
@@ -0,0 +1,28 @@
+package com.cgkim.myboard.vo.board;
+
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+public class BoardSearchRequest {
+    private Integer page; // 페이지
+    private String keyword; // 검색어
+    private Integer categoryId; // 카테고리
+    private Date fromDate; // 검색 시작 날짜
+    private Date toDate;  // 검색 끝 날짜
+
+    public BoardSearchRequest(Integer page, String keyword, Integer categoryId, Date fromDate, Date toDate) {
+        this.page = (page == null || page < 1) ? 1 : page;
+        this.keyword = keyword;
+        this.categoryId = categoryId;
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+    }
+
+    public final int limit = 10; // 한 페이지에 표시할 게시물 수
+
+    public int getOffset() {
+        return (page - 1) * limit;
+    }
+}
